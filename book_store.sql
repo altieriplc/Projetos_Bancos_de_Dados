abe4e06e0168305516f73a4a5db79e200e84d6f9
Altieri Palorca <altierirpalorca@gmail.com>
2024-09-24 20:21:31 -0300

    inserindo dados

--- book_store.sql
+++ book_store.sql
@@ -61,8 +61,8 @@ CREATE TABLE books_categories(
     id INT AUTO_INCREMENT PRIMARY KEY,
     book_id INT NOT NULL,
     category_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (book_id) REFERENCES books(id),
-    CONSTRAINT FOREIGN KEY (category_id) REFERENCES categories(id)
+    CONSTRAINT FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
 )
 
 ALTER TABLE books_categories
@@ -90,4 +90,67 @@ CREATE TABLE categories(
 
 SELECT * FROM categories;
 
-------------------------------------------------------------------------------------------------
+---------------------------------------INSERÇÃO DE DADOS--------------------------------------
+
+INSERT INTO authors(name,bio) 
+    VALUES
+        ("Eiichiro Oda", "Eiichiro Oda em um mangaká conhecido pela criação do mangá One Piece."),
+        ("Osvaldo Silva", "Autor e compositor brasileiro."),
+        ("J.K. Rowling", "J.K. Rowling é a autora de sucesso mundial de Harry Potter, uma série de sete livros que conta a história de um jovem bruxo chamado Harry Potter e seus amigos Hermione Granger e Ron Weasley, que enfrentam desafios para destruir o poderoso e malévolo bruxo Voldemort."),
+        ("George R.R. Martin", "George R.R. Martin é o autor de A Song of Ice and Fire, uma série de livros que conta a história de um mundo de fantasia onde reinam guerras constantes entre famílias reais e sobrenaturais."),
+        ("Agatha Christie", "Agatha Christie é uma das escritoras de mistério mais populares de todos os tempos, conhecida por suas histórias envolventes e personagens complexos."),
+        ("Stephenie Meyer", "Stephenie Meyer é a autora de Twilight, uma série de livros que conta a história de uma jovem que se apaixona por um vampiro."),
+        ("Dan Brown", "Dan Brown é o autor de Dan Brown, uma série de livros que conta a história de um simbologista que investiga uma conspiração global."),
+        ("J.R.R. Tolkien", "J.R.R. Tolkien é o autor de O Senhor dos Anéis, uma série de livros que conta a história de um mundo de fantasia onde reinam guerras constantes entre humanos e elfos.");
+
+
+insert into books(name, pages, author_id)
+    values
+        ("One Piece", 100, 1),
+        ("Osvaldo e a magia de Oz", 100, 2),
+        ("Harry Potter e a pedra filosofal", 100, 3),
+        ("Jogos Vorazes", 100, null),
+        ("A Sociedade do Anel", 100, 6),
+        ("A Cidade dos Anéis", 100, 6),
+        ("A Cidade dos Anéis", 100, 6);
+
+
+insert into categories(name)
+    values
+        ("Ação"),
+        ("Aventura"),
+        ("Fantasia"),
+        ("Romance"),
+        ("Terror"),
+        ("Suspense"),
+        ("Ficção Científica");
+        
+
+set @categoria_aventura_id = (SELECT id from categories where name = "Aventura");
+
+insert ignore into books_categories(book_id, category_id)
+    values
+        ((select id from books where name = "One Piece"), @categoria_aventura_id),
+        ((select id from books where name = "Osvaldo e a magia de Oz"), @categoria_aventura_id),
+        ((select id from books where name = "Harry Potter e a pedra filosofal"), @categoria_aventura_id),
+        ((select id from books where name = "Jogos Vorazes"), @categoria_aventura_id),
+        ((select id from books where name = "A Sociedade do Anel"), @categoria_aventura_id),
+        ((select id from books where name = "A Cidade dos Anéis"), @categoria_aventura_id);
+
+
+
+set @categoria_fantasia_id = (SELECT id from categories where name = "Fantasia");
+insert ignore into books_categories(book_id, category_id)
+    values
+        ((select id from books where name = "A Cidade dos Anéis"), @categoria_fantasia_id);
+
+
+set @categoria_manga_id = (SELECT id from categories where name = "Manga");
+insert ignore into books_categories(book_id, category_id)
+    values
+        ((select id from books where name = "One Piece"), @categoria_manga_id);
+
+
+insert into contact_infos(phone, email, author_id)
+    values
+        ("1234567890", "osvaldo@gmail.com", 2);
